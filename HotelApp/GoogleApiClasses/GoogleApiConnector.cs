403008ebffe4fac8c6158dd6edf6084275f618fc
maciejbak86 <maciej_bak@wp.pl>
2016-09-23 17:52:48 +0200

    Added GoogleApiConnector class

---
+++ HotelApp/GoogleApiClasses/GoogleApiConnector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotelApp.GoogleApiManager
+{
+    public class GoogleApiConnector
+    {
+        const string GoogleApiKey = "AIzaSyBkX7Ln3_XxTvuFwrLv3bAiMEL5W1dwa7I";
+
+        //var address = "123 something st, somewhere";
+        //var requestUri = string.Format("http://maps.googleapis.com/maps/api/geocode/xml?address={0}&sensor=false", Uri.EscapeDataString(address));
+
+        //var request = WebRequest.Create(requestUri);
+        //var response = request.GetResponse();
+        //var xdoc = XDocument.Load(response.GetResponseStream());
+
+        //var result = xdoc.Element("GeocodeResponse").Element("result");
+        //var locationElement = result.Element("geometry").Element("location");
+        //var lat = locationElement.Element("lat");
+        //var lng = locationElement.Element("lng");
+    }
+}
